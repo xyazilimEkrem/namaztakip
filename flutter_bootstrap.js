b57ed6bff307547298b979543af8ed1160bf048f
Admin <ekrem@x-yazilime.com>
2026-07-30 22:44:25 +0300

    fix: intl paketi 0.20.2 versiyonuna guncellendi

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4111764244" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1899889290" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
